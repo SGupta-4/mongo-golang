da2e3a0c272afd5b318af47c666d10b95103dcf7
SGupta-4 <sarthakgupta4119@gmail.com>
2025-06-23 21:31:25 +0530

    1

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/SGupta-4/mongo-golang/controllers"
+	"github.com/julienschmidt/httprouter"
+	"gopkg.in/mgo.v2"
+)
+
+func main() {
+	r := httprouter.New()
+	uc := controllers.NewUserController(getSession())
+	r.GET("/user/:id", uc.GetUser)
+	r.POST("/user", uc.CreateUser)
+	r.DELETE("/user/:id", uc.DeleteUser)
+	http.ListenAndServe(":8080", r)
+}
+func getSession() *mgo.Session {
+	s, err := mgo.Dial("mongodb://localhost:27017")
+	if err != nil {
+		panic(err)
+	}
+	return s
+}
